eaaea60f3d52e4e3e7a368c1d74f9b041105c207
MrFreedy <lennea@3il.fr>
2024-04-05 14:00:20 +0200

    TaskView created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import view.TaskView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, TP!");
+        TaskView taskView = new TaskView();
     }
 }
---
+++ src/view/TaskView.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.*;
+
+public class TaskView {
+
+    public TaskView() {
+        JFrame frame = new JFrame("Task Manager");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+}
