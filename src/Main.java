d181671e1d8dc017f3e7536c4bf63a55393cf1d0
MrFreedy <lennea@3il.fr>
2024-04-05 14:23:03 +0200

    all main menu UI done

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,19 @@ import controller.TaskController;
 import model.TaskModel;
 import view.TaskView;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         TaskModel taskModel = new TaskModel();
         TaskView taskView = new TaskView(taskModel);
         TaskController taskController = new TaskController(taskModel, taskView);
+
+
+        JFrame frame = new JFrame("Gestionnaire de tâches");
+        frame.add(taskView);
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
--- src/view/TaskView.java
+++ src/view/TaskView.java
@@ -3,13 +3,53 @@ package view;
 import model.TaskModel;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TaskView extends JPanel{
     private TaskModel model;
     public TaskView(TaskModel model) {
-        JFrame frame = new JFrame("Task Manager");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setVisible(true);
+        super();
+        this.model = model;
+        setBackground(Color.WHITE);
+
+        JLabel title = new JLabel("Gestionnaire de tâches");
+        title.setSize(200, 30);
+
+        JPanel panelV = new JPanel();
+        BoxLayout boxLayoutV = new BoxLayout(panelV, BoxLayout.Y_AXIS);
+        panelV.setBackground(Color.WHITE);
+        panelV.setLayout(boxLayoutV);
+
+
+        JPanel panelH = new JPanel();
+        BoxLayout boxLayoutH = new BoxLayout(panelH, BoxLayout.X_AXIS);
+        panelH.setBackground(Color.WHITE);
+        panelH.setLayout(boxLayoutH);
+
+        JButton createTask = new JButton("Créer une tâche");
+
+
+        JButton modifyTask = new JButton("Modifier une tâche");
+
+
+        JButton deleteTask = new JButton("Supprimer une tâche");
+
+
+        JButton listTask = new JButton("Lister les tâches");
+
+
+        JButton exit = new JButton("Quitter");
+        exit.addActionListener(e -> {
+            System.exit(0);
+        });
+
+        panelV.add(title);
+        panelV.add(panelH);
+        panelH.add(createTask);
+        panelH.add(modifyTask);
+        panelH.add(deleteTask);
+        panelH.add(listTask);
+        panelH.add(exit);
+        add(panelV);
     }
 }
