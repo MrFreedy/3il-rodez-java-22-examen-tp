da08cbd63ed8aeff4dee48ed43818b636416929c
MrFreedy <lennea@3il.fr>
2024-04-05 13:54:48 +0200

    Task class created

---
+++ src/model/Task.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.util.Objects;
+
+public class Task {
+    int id;
+    String titre;
+    String description;
+    String dateEchance;
+    String priorite;
+
+    public Task(int id, String titre, String description, String dateEchance, String priorite) {
+        this.id = id;
+        this.titre = titre;
+        this.description = description;
+        this.dateEchance = dateEchance;
+        this.priorite = priorite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateEchance() {
+        return dateEchance;
+    }
+
+    public void setDateEchance(String dateEchance) {
+        this.dateEchance = dateEchance;
+    }
+
+    public String getPriorite() {
+        return priorite;
+    }
+
+    public void setPriorite(String priorite) {
+        this.priorite = priorite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && Objects.equals(titre, task.titre) && Objects.equals(description, task.description) && Objects.equals(dateEchance, task.dateEchance) && Objects.equals(priorite, task.priorite);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, titre, description, dateEchance, priorite);
+    }
+}
